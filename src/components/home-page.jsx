3f9129be0c9d61e86b8e29d99a3d9081c2802576
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2023-12-25 13:58:57 +0700

    homepage layout progress #2

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -26,23 +26,65 @@ function HomePage() {
           <span>Data Science</span><span>Data Engineering</span><span>Cloud Engineering</span>
         </div>
         <div className="content-div">
-        <div className="about-content">
-            <div className="about-top">
+                  <div className="about-content">
+                  <div className="about-title">
+                              <h1>About</h1>
+                          </div>
+                      <div className="about-top">
                 <div className="about-content-img">
                 <p>this is for image</p>
                 </div>
                 <div className="about-content-p">
                 <span>About Me</span>
-                              <p>this is for description</p>
-                              <button>Button</button>
+                    <p>this is for description</p>
+                    <button>Button</button>
                 </div>
             </div>
             </div>
-            <div className="project-content">
-                <p>this is for project</p>
+                  <div className="project-content">
+                      <div className="project-title">
+                          <h1>Projects</h1>
+                      </div>
+                      <div className="project">
+                          <div className="project-image">
+                              <p>this is for image</p>
+                          </div>
+                          <div className="project-image-desc">
+                              <h3>Project 1</h3>
+                              <p>This is description</p>
+                          </div>
+                      </div>
+                      <div className="project">
+                      <div className="project-image">
+                              <p>this is for image</p>
+                          </div>
+                          <div className="project-image-desc">
+                              <h3>Project 2</h3>
+                              <p>This is description</p>
+                          </div>
+                      </div>
+                      <div className="project">
+                      <div className="project-image">
+                              <p>this is for image</p>
+                          </div>
+                          <div className="project-image-desc">
+                              <h3>Project 3</h3>
+                              <p>This is description</p>
+                          </div>
+                      </div>
+            </div>
+            <div className="project-link">
+                <button>Button</button>
             </div>
             <div className="resume-content">
-                <p>this is for resume</p>
+                      <div className="resume-desc">
+                        <h3>Resume</h3>
+                          <p>This is for description</p>
+                          <button>Button</button>
+                      </div>
+                      <div className="resume-img">
+                          <p>This is for image</p>
+                      </div>
             </div>
             <div className="contact-content">
                 <p>this is for contact</p>
