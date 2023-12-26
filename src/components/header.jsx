0905d2fae1cc8aae0932ef54c373c2ca57b431ee
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2023-12-26 15:04:44 +0700

    added header dropdown menu and responsivity

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,34 +1,41 @@
 //Imports from React
-import React from 'react';
+import React, { useState } from 'react';
 
 //Imports from files
 import '../css/header.css';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+  
     return (
-        <header className="header-page-container">
-            <div className="header-page-body">
-                <div className="header-left">
-                    <div className="header-logo">
-                        <p>Logo</p>
-                    </div>
-                </div>
-                <div className="header-right">
-                    <ul className="header-nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Resume</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                    <div className="header-button">
-        <button className="header-email">example@gmail.com</button>
-    </div>
-</div>
-
-            </div>
-        </header>
+      <header className="header-container flex justify-between items-center p-5 bg-blue-500">
+        <div className="header-logo flex-grow">
+          <p className="text-white">Logo</p>
+        </div>
+        <div className="header-right lg:w-1/2 w-full justify-end flex">
+          <div className="menu-container">
+            <button 
+              className="header-navbar-button lg:hidden block" 
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? 'Close' : 'Menu'}
+            </button>
+            {isOpen && (
+              <ul className="header-nav lg:flex items-center">
+                <li className="header-button mr-12"><a href="#" className="text-white">Home</a></li>
+                <li className="header-button mr-12"><a href="#" className="text-white">About</a></li>
+                <li className="header-button mr-12"><a href="#" className="text-white">Projects</a></li>
+                <li className="header-button mr-12"><a href="#" className="text-white">Resume</a></li>
+                <li className="header-button mr-12"><a href="#" className="text-white">Contact</a></li>
+                <li className="header-button mr-12">
+                  <button className="header-mail-button bg-white text-blue-500 px-5 py-2 rounded-full">example@gmail.com</button>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+      </header>
     );
-}
+  }
 
 export default Header;
