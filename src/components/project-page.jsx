50abf5db58629dfaeedc5ed3b001ac9f2724bfc6
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2023-12-29 20:12:38 +0700

    created layout for project page

--- src/components/project-page.jsx
+++ src/components/project-page.jsx
@@ -2,13 +2,35 @@
 import React from 'react';
 
 //Imports from files
+import '../css/project-page.css';
 import Header from './header';
+import Footer from './footer';
 
 function ProjectPage() {
     return (
-        <div className="home-page-container">
-            <div className="home-page-body">
-                {/* add your body content here */}
+        <div className="project-page-container">
+            <div>
+                <Header />
+            </div>
+            <div className="project-page-body">
+                <div className="project-body-title">
+                    <span>Projects</span>
+                </div>
+                <div className="project-data-science">
+                    <span>Data Science Projects</span>
+
+                </div>
+                <div className="project-data-engineering">
+                    <span>Data Engineering Projects</span>
+
+                </div>
+                <div className="project-cloud-computation">
+                    <span>Cloud Computation Projects</span>
+
+                </div>
+            </div>
+            <div>
+                <Footer />
             </div>
         </div>
     );
